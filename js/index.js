525db10890e293eff4edbfad9a67136142bc712d
AntNegron <anthonyanegron@gmail.com>
2021-09-27 18:56:15 -0400

    completed the basic update of the site for it to match the design-files

--- js/index.js
+++ js/index.js
@@ -40,3 +40,73 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// update for nav
+const navLink1 = document.querySelector('nav a:nth-child(1)');
+navLink1.textContent = siteContent['nav'] ['nav-item-1'];
+const navLink2 = document.querySelector('nav a:nth-child(2)');
+navLink2.textContent = siteContent['nav'] ['nav-item-2'];
+const navLink3 = document.querySelector('nav a:nth-child(3)');
+navLink3.textContent = siteContent['nav'] ['nav-item-3'];
+const navLink4 = document.querySelector('nav a:nth-child(4)');
+navLink4.textContent = siteContent['nav'] ['nav-item-4'];
+const navLink5 = document.querySelector('nav a:nth-child(5)');
+navLink5.textContent = siteContent['nav'] ['nav-item-5'];
+const navLink6 = document.querySelector('nav a:nth-child(6)');
+navLink6.textContent = siteContent['nav'] ['nav-item-6'];
+
+
+
+// cta update
+const ctaImg = document.querySelector('#cta-img');
+ctaImg.src = 'img/header-img.png';
+
+const ctaText = document.querySelector('.cta-text h1');
+ctaText.innerHTML = 'Dom<br>Is<br>Awesome<br>';
+
+const ctaButton = document.querySelector('.cta-text button');
+ctaButton.textContent = 'Get Started';
+
+
+
+// main-content update
+const featureH4 = document.querySelector('.top-content:nth-child(1) h4');
+featureH4.textContent = siteContent['main-content']['features-h4'];
+const featureContent = document.querySelector('.top-content:nth-child(1) p');
+featureContent.textContent = siteContent['main-content']['features-content'];
+
+const aboutH4 = document.querySelector('.top-content .text-content:nth-child(2) h4');
+aboutH4.textContent = siteContent['main-content']['about-h4'];
+const aboutContent = document.querySelector('.top-content .text-content:nth-child(2) p');
+aboutContent.textContent = siteContent['main-content']['about-content'];
+
+const middleImg = document.querySelector('#middle-img');
+middleImg.src = 'img/mid-page-accent.jpg';
+
+const serviceH4 = document.querySelector('.bottom-content .text-content:nth-child(1) h4');
+serviceH4.textContent = siteContent['main-content']['services-h4'];
+const serviceContent = document.querySelector('.bottom-content .text-content:nth-child(1) p');
+serviceContent.textContent = siteContent['main-content']['services-content'];
+
+const productH4 = document.querySelector('.bottom-content .text-content:nth-child(2) h4');
+productH4.textContent = siteContent['main-content']['product-h4'];
+const productContent = document.querySelector('.bottom-content .text-content:nth-child(2) p');
+productContent.textContent = siteContent['main-content']['product-content'];
+
+
+
+// contact update
+ const contactH4 = document.querySelector('.contact h4');
+ contactH4.textContent = siteContent['contact']['contact-h4'];
+ const contactAddress = document.querySelector('.contact p:nth-child(2)')
+ contactAddress.innerHTML = '123 Way 456 Street<br>Somewhere, USA';
+ const contactPhone = document.querySelector('.contact p:nth-child(3)');
+ contactPhone.textContent = siteContent['contact']['phone'];
+ const contactEmail = document.querySelector('.contact p:nth-child(4)');
+ contactEmail.textContent = siteContent['contact']['email'];
+
+
+
+//  footer update
+const footer = document.querySelector('footer');
+footer.textContent = siteContent['footer']['copyright'];
